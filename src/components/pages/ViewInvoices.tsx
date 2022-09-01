f6e5eb9a29ee92d45ede96ddc18d668302619724
Michael Hume <michael.a.hume@pm.me>
2022-09-01 17:21:26 -0400

    feat(pages): - View invoice functional

--- src/components/pages/ViewInvoices.tsx
+++ src/components/pages/ViewInvoices.tsx
@@ -1,11 +1,41 @@
 import React from "react";
-import {MerchantTableColHeaders, Table, TableBody} from '../shared/tables'
+import {Table, TableBody, TableCell, TableRow, MerchantTableColHeaders} from '../shared/tables'
+import {CryptoCurrency} from "../../types/currency";
+import Paragraph from "../shared/texts/Paragraph";
+import {useCurrency} from "../../hooks/useCurrency";
+import useInvoices from "../../hooks/useInvoices";
 
 const ViewInvoices = () => {
+    const {invoices} = useInvoices();
+    const {formatCryptoCurrency, formatCryptoPriceInUSD} = useCurrency();
+
     return (
         <Table>
             <MerchantTableColHeaders/>
-            <TableBody></TableBody>
+            <TableBody>
+                {invoices && invoices.map(({id, merchant, item, payment}) => (
+                        <TableRow key={id}>
+                            <TableCell flex={1}>
+                                <Paragraph text={merchant} size="16px" weight={500} />
+                            </TableCell>
+                            <TableCell flex={1}>
+                                <Paragraph text={item} size="16px" weight={500} />
+                            </TableCell>
+                            <TableCell flex={1} textAlign="right">
+                                <Paragraph text={formatCryptoCurrency(payment.amount)} size="16px" weight={500} />
+                            </TableCell>
+                            <TableCell flex={1}>{CryptoCurrency[payment.currency]}</TableCell>
+                            <TableCell flex={1} textAlign="right">
+                                {formatCryptoPriceInUSD(payment.currency) ?? '-'}
+                            </TableCell>
+                            <TableCell flex={1} textAlign="right">
+                                {formatCryptoPriceInUSD(payment.currency, payment.amount) ?? '-'}
+                            </TableCell>
+                            <TableCell flex={0.2}/>
+                        </TableRow>
+                    )
+                )}
+            </TableBody>
         </Table>
     );
 }
